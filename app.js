4efa4cfb773ae613317c75dc89928aba16297c7f
joon1ta <armestojona@gmail.com>
2021-01-29 00:20:46 -0300

    Port

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const path = require('path');
 
 app.use('/static', express.static(__dirname + '/public'))
 
-app.listen(3000, () => {
-    console.log('Servidor Corriendo')
+app.listen(process.env.PORT || 3000
+    , () => {
+   
 })
 
 app.get('/home', (req, res) => {
