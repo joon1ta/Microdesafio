c43fab62f6d89bac5a25200b7028b3ded5fd7891
joon1ta <armestojona@gmail.com>
2021-01-28 22:41:10 -0300

    Ultima dia: terminado las Routes, HTMLS y CSS

--- app.js
+++ app.js
@@ -11,3 +11,15 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 })
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+})
